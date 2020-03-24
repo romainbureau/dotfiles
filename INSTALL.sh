6d88451d6c7f76535d65485397cfa1958fd17b0d
Romain Bureau <rb@hio.fr>
2020-03-24 08:36:30 +0100

    chore(sublime-settings): move sublime-settings

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,6 +9,6 @@ ln -sf $HOME/workspace/dotfiles/gitconfig $HOME/.gitconfig
 ln -sf $HOME/workspace/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/workspace/dotfiles/bash_profile $HOME/.bash_profile
 ln -sf $HOME/workspace/dotfiles/bashrc $HOME/.bashrc
-ln -sf $HOME/workspace/dotfiles/Preferences.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-ln -sf $HOME/workspace/dotfiles/Package\ Control.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
+ln -sf $HOME/workspace/dotfiles/config/sublime-text-3/Packages/User/Preferences.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -sf $HOME/workspace/dotfiles/config/sublime-text-3/Packages/User/Package\ Control.sublime-settings $HOME/.config/sublime-text-3/Packages/User/Package\ Control.sublime-settings
 
