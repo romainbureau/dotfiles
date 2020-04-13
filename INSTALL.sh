c50a0c56813010711ebc83dabebd2ad026b1313e
Romain Bureau <rb@hio.fr>
2020-04-13 17:15:46 +0200

    chore(redshift): use config file

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,6 +4,7 @@ ln -sf $HOME/workspace/dotfiles/config/sway $HOME/.config/
 ln -sf $HOME/workspace/dotfiles/config/wofi $HOME/.config/
 ln -sf $HOME/workspace/dotfiles/config/waybar $HOME/.config/
 ln -sf $HOME/workspace/dotfiles/config/alacritty $HOME/.config/
+ln -sf $HOME/workspace/dotfiles/config/redshift $HOME/.config/
 ln -sf $HOME/workspace/dotfiles/config/kanshi $HOME/.config/
 ln -sf $HOME/workspace/dotfiles/gitconfig $HOME/.gitconfig
 ln -sf $HOME/workspace/dotfiles/tmux.conf $HOME/.tmux.conf
