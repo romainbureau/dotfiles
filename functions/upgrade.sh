237412d643243640fa9d000b55c178d694ee2444
Romain Bureau <rb@hio.fr>
2020-10-23 10:34:09 +0200

    feat(functions/upgrade): add delta

--- functions/upgrade.sh
+++ functions/upgrade.sh
@@ -3,14 +3,28 @@
 BIN_DIR="$HOME/bin"
 
 function upgrade_terraform() {
-  local VERSION="${1}"
-  if [[ -z ${VERSION} ]]; then
-    echo "Please, specify a version (example: 0.13.4)"
-  else
-    local URL="https://releases.hashicorp.com/terraform/0.13.4/terraform_${VERSION}_linux_amd64.zip"
-    local FILENAME="$(basename ${URL})"
-    echo "${URL}"
-    curl -o "/tmp/${FILENAME}" "${URL}"
-    unzip "/tmp/${FILENAME}" -d "${BIN_DIR}"
-  fi
+	local VERSION="${1}"
+	if [[ -z ${VERSION} ]]; then
+		echo "Please, specify a version (example: 0.13.4)"
+	else
+		local URL="https://releases.hashicorp.com/terraform/0.13.4/terraform_${VERSION}_linux_amd64.zip"
+		local FILENAME="$(basename ${URL})"
+		echo "${URL}"
+		curl -o "/tmp/${FILENAME}" "${URL}"
+		unzip "/tmp/${FILENAME}" -d "${BIN_DIR}"
+	fi
+}
+
+function upgrade_delta() {
+	local VERSION="${1}"
+	if [[ -z ${VERSION} ]]; then
+		echo "Please, specify a version (example: 0.4.4)"
+	else
+		local URL="https://github.com/dandavison/delta/releases/download/${VERSION}/delta-${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+		local FILENAME="$(basename ${URL})"
+		echo "${URL}"
+		curl -L -o "/tmp/${FILENAME}" "${URL}"
+		tar -xvzf "/tmp/${FILENAME}" -C "/tmp" "delta-${VERSION}-x86_64-unknown-linux-gnu/delta"
+		mv "/tmp/delta-${VERSION}-x86_64-unknown-linux-gnu/delta" "${BIN_DIR}"
+	fi
 }
