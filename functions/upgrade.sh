10afe6de73c0c682bba70a0cd3c634ebd5b420bd
Romain Bureau <rb@hio.fr>
2020-11-04 09:42:32 +0100

    chore: add upgrade_kubectl function + misc

--- functions/upgrade.sh
+++ functions/upgrade.sh
@@ -28,3 +28,15 @@ function upgrade_delta() {
 		mv "/tmp/delta-${VERSION}-x86_64-unknown-linux-gnu/delta" "${BIN_DIR}"
 	fi
 }
+
+function upgrade_kubectl() {
+  local VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+  if [[ -z ${VERSION} ]]; then
+    echo "Please, specify a version (example: 1.19.0)"
+  else
+    local URL="https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kubectl"
+    local FILENAME="$(basename ${URL})"
+    echo "${URL}"
+    curl -L -o "/${BIN_DIR}/${FILENAME}" "${URL}"
+  fi
+}
