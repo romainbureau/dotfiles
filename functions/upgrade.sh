526a5097375dfb885ef880a547f2c9a33f59d33e
Romain Bureau <rb@hio.fr>
2020-11-04 10:55:46 +0100

    chore: add some tools

--- functions/upgrade.sh
+++ functions/upgrade.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+function upgrade_all() {
+  upgrade_terraform "0.13.4"
+  upgrade_delta "0.4.4"
+  upgrade_kubectl "1.19.3"
+  upgrade_kubectx "0.9.1"
+  upgrade_kubens "0.9.1"
+  upgrade_kail "0.15.0"
+  upgrade_fzf "0.24.2"
+}
+
+
 BIN_DIR="$HOME/bin"
 
 function upgrade_terraform() {
@@ -7,9 +18,8 @@ function upgrade_terraform() {
 	if [[ -z ${VERSION} ]]; then
 		echo "Please, specify a version (example: 0.13.4)"
 	else
-		local URL="https://releases.hashicorp.com/terraform/0.13.4/terraform_${VERSION}_linux_amd64.zip"
+		local URL="https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip"
 		local FILENAME="$(basename ${URL})"
-		echo "${URL}"
 		curl -o "/tmp/${FILENAME}" "${URL}"
 		unzip "/tmp/${FILENAME}" -d "${BIN_DIR}"
 	fi
@@ -22,21 +32,77 @@ function upgrade_delta() {
 	else
 		local URL="https://github.com/dandavison/delta/releases/download/${VERSION}/delta-${VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 		local FILENAME="$(basename ${URL})"
-		echo "${URL}"
 		curl -L -o "/tmp/${FILENAME}" "${URL}"
 		tar -xvzf "/tmp/${FILENAME}" -C "/tmp" "delta-${VERSION}-x86_64-unknown-linux-gnu/delta"
 		mv "/tmp/delta-${VERSION}-x86_64-unknown-linux-gnu/delta" "${BIN_DIR}"
+
 	fi
 }
 
 function upgrade_kubectl() {
-  local VERSION="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
+  local VERSION="${1}"
   if [[ -z ${VERSION} ]]; then
     echo "Please, specify a version (example: 1.19.0)"
   else
-    local URL="https://storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/amd64/kubectl"
-    local FILENAME="$(basename ${URL})"
-    echo "${URL}"
-    curl -L -o "/${BIN_DIR}/${FILENAME}" "${URL}"
+    local FILENAME="kubectl"
+    local URL="https://storage.googleapis.com/kubernetes-release/release/v${VERSION}/bin/linux/amd64/${FILENAME}"
+    curl -L -o "${BIN_DIR}/${FILENAME}" "${URL}"
+    chmod +x "${BIN_DIR}/${FILENAME}"
+  fi
+}
+
+function upgrade_kubectx() {
+  local VERSION="${1}"
+  if [[ -z ${VERSION} ]]; then
+    echo "Please, specify a version (example: 0.9.1)"
+  else
+    local FILENAME="kubectx"
+    local URL="https://github.com/ahmetb/kubectx/releases/download/v${VERSION}/${FILENAME}_v${VERSION}_linux_x86_64.tar.gz"
+    curl -L -o "/tmp/${FILENAME}" "${URL}"
+    tar -xvzf "/tmp/${FILENAME}" -C "/tmp" "${FILENAME}"
+    mv "/tmp/${FILENAME}" "${BIN_DIR}/${FILENAME}"
+    chmod +x "${BIN_DIR}/${FILENAME}"
+  fi
+}
+
+function upgrade_kubens() {
+  local VERSION="${1}"
+  if [[ -z ${VERSION} ]]; then
+    echo "Please, specify a version (example: 0.9.1)"
+  else
+    local FILENAME="kubens"
+    local URL="https://github.com/ahmetb/kubectx/releases/download/v${VERSION}/${FILENAME}_v${VERSION}_linux_x86_64.tar.gz"
+    curl -L -o "/tmp/${FILENAME}" "${URL}"
+    tar -xvzf "/tmp/${FILENAME}" -C "/tmp" "${FILENAME}"
+    mv "/tmp/${FILENAME}" "${BIN_DIR}/${FILENAME}"
+    chmod +x "${BIN_DIR}/${FILENAME}"
+  fi
+}
+
+function upgrade_kail() {
+  local VERSION="${1}"
+  if [[ -z ${VERSION} ]]; then
+    echo "Please, specify a version (example: 0.15.0)"
+  else
+    local FILENAME="kail"
+    local URL="https://github.com/boz/kail/releases/download/v${VERSION}/${FILENAME}_${VERSION}_linux_amd64.tar.gz"
+    curl -L -o "/tmp/${FILENAME}" "${URL}"
+    tar -xvzf "/tmp/${FILENAME}" -C "/tmp" "${FILENAME}"
+    mv "/tmp/${FILENAME}" "${BIN_DIR}/${FILENAME}"
+    chmod +x "${BIN_DIR}/${FILENAME}"
+  fi
+}
+
+function upgrade_fzf() {
+  local VERSION="${1}"
+  if [[ -z ${VERSION} ]]; then
+    echo "Please, specify a version (example: 0.24.2)"
+  else
+    local FILENAME="fzf"
+    local URL="https://github.com/junegunn/${FILENAME}/releases/download/${VERSION}/${FILENAME}-${VERSION}-linux_amd64.tar.gz"
+    curl -L -o "/tmp/${FILENAME}" "${URL}"
+    tar -xvzf "/tmp/${FILENAME}" -C "/tmp" "${FILENAME}"
+    mv "/tmp/${FILENAME}" "${BIN_DIR}/${FILENAME}"
+    chmod +x "${BIN_DIR}/${FILENAME}"
   fi
 }
